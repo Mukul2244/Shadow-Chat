076f54ad99def48386408a7da85e6543ec0a5f58
Mukul2244 <mukulsharma1594@gmail.com>
2024-07-28 19:36:59 +0530

    add accept,get and send messages routes

---
+++ src/app/api/send-messages/route.ts
@@ -0,0 +1,41 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User.model";
+import { Message } from "@/models/User.model";
+
+export async function POST(request:Request){
+    await dbConnect()
+   const {username,content}= await request.json()
+   try {
+    const user =await UserModel.findOne({username})
+    if(!user){
+        return Response.json({
+            success: false,
+            message: "User not found"
+        }, { status: 404 })
+    }
+    // Is user accepting the messages
+    if(!user.isAcceptingMessage){
+        return Response.json({
+            success: false,
+            message: "User is not accepting the messages"
+        }, { status: 403 })
+    }
+    const newMessage={content,createdAt:new Date()} 
+    user.messages.push(newMessage as Message)
+    await user.save()
+    return Response.json({
+        success: true,
+        message: "Message sent successfully"
+    }, { status: 200 })
+    
+   } catch (error) {
+    console.log("Error in adding the message")
+
+    return Response.json({
+        success: false,
+        message: "Interval server error..."
+    }, { status: 500 })
+}
+}
+
+
